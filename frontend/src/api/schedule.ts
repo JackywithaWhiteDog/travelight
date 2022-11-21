a101425e0a268f29c614e46bd99301c6afe17ecd
henrychienhy <b07902023@ntu.edu.tw>
2022-11-21 13:32:59 +0800

    fix(frontend): fix dispath typo

--- frontend/src/api/schedule.ts
+++ frontend/src/api/schedule.ts
@@ -1,8 +1,8 @@
-import { StoreDispath } from '../store'
+import { StoreDispatch } from '../store'
 import { reorderSchedule } from '../store/reducers/attractions'
 import { Attraction } from '../types'
 
-export const optimizeSchedule = async (schedule: Attraction[], dispath: StoreDispath): Promise<void> => {
+export const optimizeSchedule = async (schedule: Attraction[], dispatch: StoreDispatch): Promise<void> => {
   const order = Array.from(Array(schedule.length).keys()).sort(() => Math.random() - 0.5)
-  dispath(reorderSchedule(order))
+  dispatch(reorderSchedule(order))
 }
