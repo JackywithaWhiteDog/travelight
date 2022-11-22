a366a17c7ecc1e4a765c73b4660e57689ef69b66
henrychienhy <b07902023@ntu.edu.tw>
2022-11-22 18:17:57 +0800

    feat(frontend.api, frontend.type): add Constraint and modify SelectableAttraction

---
+++ frontend/src/api/mockData.ts
@@ -0,0 +1,92 @@
+export const mockSelectableAttractionList = [
+  {
+    name: '台北車站',
+    location: { longitude: 121.515336, latitude: 25.0462548 },
+    placeId: '1234',
+    rating: 4.1,
+    isSelected: false,
+    constraint: {
+      openingTimes: [
+        0
+      ],
+      closeTimes: [
+        0
+      ],
+      stayTime: 0,
+      transportation: 'driving'
+    },
+    address: '100台北市中正區'
+  },
+  {
+    name: '華山1914文化創意產業園區',
+    location: { longitude: 121.5267377, latitude: 25.0445075 },
+    placeId: '5678',
+    rating: 4.4,
+    isSelected: false,
+    constraint: {
+      openingTimes: [
+        0
+      ],
+      closeTimes: [
+        0
+      ],
+      stayTime: 0,
+      transportation: 'driving'
+    },
+    address: '100台北市中正區八德路一段1號'
+  },
+  {
+    name: '國家音樂廳',
+    location: { longitude: 121.5176232, latitude: 25.037252 },
+    placeId: '4123',
+    rating: 4.7,
+    isSelected: false,
+    constraint: {
+      openingTimes: [
+        0
+      ],
+      closeTimes: [
+        0
+      ],
+      stayTime: 0,
+      transportation: 'driving'
+    },
+    address: '100台北市中正區中山南路21-1號'
+  },
+  {
+    name: '龍門客棧餃子館 (林森店)',
+    location: { longitude: 121.5221428, latitude: 25.0396153 },
+    placeId: '5682',
+    rating: 4.4,
+    isSelected: false,
+    constraint: {
+      openingTimes: [
+        0
+      ],
+      closeTimes: [
+        0
+      ],
+      stayTime: 0,
+      transportation: 'driving'
+    },
+    address: '100台北市中正區林森南路61巷19號'
+  },
+  {
+    name: '興波咖啡旗艦店',
+    location: { longitude: 121.5271228, latitude: 25.0422281 },
+    placeId: '3228',
+    rating: 4.4,
+    isSelected: false,
+    constraint: {
+      openingTimes: [
+        0
+      ],
+      closeTimes: [
+        0
+      ],
+      stayTime: 0,
+      transportation: 'driving'
+    },
+    address: '100台北市中正區忠孝東路二段27號'
+  }
+]
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,11 +8,27 @@ export interface Region {
   location: Location
 }
 
+export interface Constraint {
+  openingTimes: number[]
+  closeTimes: number[]
+  stayTime: number
+  transportation: string
+}
+
 export interface Attraction extends Region {
   placeId: string
+  constraint: Constraint
   rating: number
+  address: string
 }
 
 export interface SelectableAttraction extends Attraction {
   isSelected: boolean
+}
+
+export interface Order {
+  order: number[]
+  arrivalTimes: number[]
+  leaveTimes: number[]
+  isValid: boolean
 }
