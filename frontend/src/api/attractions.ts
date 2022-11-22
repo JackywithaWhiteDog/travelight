460d9fd717a1b4546b9153af55b4c43f383b7b9a
henrychienhy <b07902023@ntu.edu.tw>
2022-11-23 00:56:10 +0800

    feat(frontend.api): add getRecommendationAPI

--- frontend/src/api/attractions.ts
+++ frontend/src/api/attractions.ts
@@ -1,9 +1,36 @@
 import { StoreDispatch } from '../store'
 import { addRecommendation } from '../store/reducers/attractions'
-import { Location, SelectableAttraction } from '../types'
-import { mockSelectableAttractionList } from './mockData'
+import { Constraint, Location, SelectableAttraction } from '../types'
+import axios from 'axios'
+
+const API_ROOT = 'http://localhost:4000/api'
+
+export const getRecommendationAPI = async (location: Location): Promise<SelectableAttraction[]> => {
+  const response = await axios.get(API_ROOT + '/nearbyAttractions', {
+    params: {
+      longtitude: location.longitude,
+      latitude: location.latitude
+    }
+  })
+  const rawdata = response.data
+  const data: SelectableAttraction[] = []
+  rawdata.forEach((rawElement: any) => {
+    const element: SelectableAttraction = {
+      placeId: rawElement.place_id as string,
+      rating: rawElement.rating as number,
+      name: rawElement.name as string,
+      location: rawElement.location as Location,
+      constraint: rawElement.constraint as Constraint,
+      address: rawElement.address as string,
+      pictureURL: rawElement.pictureURL as string,
+      isSelected: false
+    }
+    data.push(element)
+  })
+  return data
+}
 
 export const getRecommendation = async (location: Location, dispatch: StoreDispatch): Promise<void> => {
-  const mockData: SelectableAttraction[] = mockSelectableAttractionList
-  dispatch(addRecommendation(mockData))
+  const data = await getRecommendationAPI(location)
+  dispatch(addRecommendation(data))
 }
