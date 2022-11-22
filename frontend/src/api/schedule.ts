588177021d12176f538e024ef5294bae32aa9f12
henrychienhy <b07902023@ntu.edu.tw>
2022-11-23 01:21:11 +0800

    feat(frontend.api): add optimizeScheduleAPI

--- frontend/src/api/schedule.ts
+++ frontend/src/api/schedule.ts
@@ -1,9 +1,20 @@
 import { StoreDispatch } from '../store'
 import { reorderSchedule } from '../store/reducers/attractions'
 import { Attraction, Order } from '../types'
-import { generateMockOrder } from './mockData'
+import axios from 'axios'
+
+const API_ROOT = 'http://localhost:4000/api'
+
+export const optimizeScheduleAPI = async (schedule: Attraction[]): Promise<Order> => {
+  const response = await axios.get(API_ROOT + '/optimize', {
+    params: {
+      data: schedule
+    }
+  })
+  return (response.data as Order)
+}
 
 export const optimizeSchedule = async (schedule: Attraction[], dispatch: StoreDispatch): Promise<void> => {
-  const order: Order = generateMockOrder(schedule)
+  const order: Order = await optimizeScheduleAPI(schedule)
   dispatch(reorderSchedule(order.order))
 }
