6d8816f0d699f83babfc3f23bcc75ec6f7a736fd
weeturn <sasquatch110@gmail.com>
2022-11-24 18:53:25 +0800

    Revert "feat: attraction card updated"
    This reverts commit 6e8ae794dc86c98876dfab0fb1fab09355dee553.

--- frontend/src/components/attractionCard.tsx
+++ frontend/src/components/attractionCard.tsx
@@ -1,66 +1,38 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { Card, CardContent, CardActions, CardMedia, Rating, Typography, Box, IconButton } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete'
+import { Card, Button, CardContent, CardActions } from '@mui/material'
 import { cancelAttraction } from '../store/reducers/attractions'
 import { SelectableAttraction } from '../types'
 
 const AttractionCard = (props: { attraction: SelectableAttraction, visibility: boolean, index: number }): React.ReactElement => {
   const dispatch = useDispatch()
-  const openingTimeHour = Math.floor(props.attraction.constraint.openingTimes[0])
-  const openingTimeMin = (props.attraction.constraint.openingTimes[0] - Math.floor(props.attraction.constraint.openingTimes[0])) * 60
-  const closingTimeHour = Math.floor(props.attraction.constraint.closingTimes[0])
-  const closingTimeMin = (props.attraction.constraint.closingTimes[0] - Math.floor(props.attraction.constraint.closingTimes[0])) * 60
-
-  function addLeadingZeros (num: number, totalLength: number): string {
-    return String(num).padStart(totalLength, '0')
-  }
 
   return (
-    <Box sx ={{ display: props.visibility ? 'relative' : 'none', position: 'relative', maxWidth: 360 }}>
+    <Card
+      sx={{
+        display: props.visibility ? 'flex' : 'none',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between'
+      }}
+    >
+      <CardContent>
+        <p>{props.attraction.name}</p>
+        <p>({props.attraction.location.latitude}, {props.attraction.location.longitude})</p>
+        <p>rating: {props.attraction.rating}</p>
+        <p>URL: {props.attraction.pictureURL}</p>
+      </CardContent>
       {
         props.attraction.isSelected &&
-        <CardActions onClick={() => dispatch(cancelAttraction(props.index))}
-        sx={{ top: 0, right: 0, zIndex: 1, position: 'absolute' }}>
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
+        <CardActions>
+          <Button
+            variant="contained"
+            onClick={() => dispatch(cancelAttraction(props.index))}
+          >
+            取消
+          </Button>
         </CardActions>
       }
-      <Card sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        borderRadius: '16px'
-      }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardContent sx={{ padding: 1, '&:last-child': { paddingBottom: 1 }, paddingLeft: 2 }}>
-            <Typography component="div" variant="h6" sx={{ fontWeight: 'bold', lineHeight: 1.4 }}>
-              {props.attraction.name}
-            </Typography>
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography variant="body2" color="text.secondary" component="div" sx ={{ paddingLeft: 0.25, paddingRight: 0.2 }}>
-                {props.attraction.rating}
-              </Typography>
-              <Rating value={props.attraction.rating} readOnly precision={0.1} size="small"/>
-            </Box>
-            <Typography variant="body2" color="text.secondary" component="div">
-              {props.attraction.address}
-            </Typography>
-            <Typography variant="body2" color="text.secondary" component="div">
-              {openingTimeHour}:{addLeadingZeros(openingTimeMin, 2)} - {closingTimeHour}:{addLeadingZeros(closingTimeMin, 2)}
-            </Typography>
-            <Typography variant="body2" color="text.secondary" component="div">
-              停留時間：{props.attraction.constraint.stayTime} 小時
-            </Typography>
-          </CardContent>
-        </Box>
-        <CardMedia
-          component="img"
-          sx={{ maxWidth: 180 }}
-          image={props.attraction.pictureURL}
-        />
-      </Card>
-    </Box>
+    </Card>
   )
 }
 
