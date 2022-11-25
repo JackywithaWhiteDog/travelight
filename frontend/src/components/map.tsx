08f76e49ee16e34e6125d806ee6a03a2e537c8ab
St-Ren <stephanieren1017@gmail.com>
2022-11-25 18:25:50 +0800

    feat-route_with_way_point

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -2,7 +2,8 @@ import {
   useJsApiLoader,
   GoogleMap,
   Marker,
-  Autocomplete
+  Autocomplete,
+  DirectionsRenderer
 } from '@react-google-maps/api'
 
 import { Input, Skeleton, Button, Box, Typography } from '@mui/material'
@@ -15,22 +16,71 @@ import AttractionPin from './attractionPin'
 const Map = (): React.ReactElement => {
   const location = useSelector((state: StoreState) => state.attractions.location)
   const recommendation = useSelector((state: StoreState) => state.attractions.recommendation, shallowEqual)
+  const [directionsResponse, setDirectionsResponse] = React.useState<google.maps.DirectionsResult | undefined>()
   if (process.env.REACT_APP_GOOGLE_MAPS_API_KEY === undefined) {
     process.env.REACT_APP_GOOGLE_MAPS_API_KEY = ''
+    console.error('No key undefined')
   }
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries: ['places']
   })
+
   const [map, setMap] = React.useState<google.maps.Map | null>(/** @type google.maps.Map */(null))
   /** const [directionsResponse, setDirectionsResponse] = React.useState<google.maps.DirectionsResult|null>((null)) */
 
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = React.useRef<HTMLInputElement>(null)
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = React.useRef<HTMLInputElement>(null)
+  const destinantionRef = React.useRef<HTMLInputElement>(null)
+  async function calculateRoute (): Promise<void> {
+    var selectedlist = []
+    for (let i = 0; i < recommendation.length; i++) {
+      if (recommendation[i].isSelected) {
+        selectedlist.push(recommendation[i])
+      }
+    }
+    if (selectedlist.length < 2) {
+      return
+    }
+    console.log(selectedlist)
+    var originpoint = new google.maps.LatLng(selectedlist[0].location.latitude, selectedlist[0].location.longitude)
+    var despoint = new google.maps.LatLng(selectedlist[selectedlist.length - 1].location.latitude, selectedlist[selectedlist.length - 1].location.longitude)
+    // eslint-disable-next-line no-undefined
+    const directionsService = new google.maps.DirectionsService()
+    if (selectedlist.length > 2) {
+      console.log('more')
+      const waypoint = selectedlist.slice(1, -1).map((rec) => {
+        return {
+          location: { lat: rec.location.latitude, lng: rec.location.longitude },
+          stopover: true
+        }
+      })
+      const results = await directionsService.route({
+
+        origin: originpoint,
+        destination: despoint,
+        waypoints: waypoint,
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING
+      })
+      setDirectionsResponse(results)
+    } else {
+      console.log('2')
+      const results = await directionsService.route({
+
+        origin: originpoint,
+        destination: despoint,
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING
+      })
+      setDirectionsResponse(results)
+    }
+  }
+
   const center = { lat: location.latitude, lng: location.longitude }
+
   if (!isLoaded) {
     return <Skeleton />
   }
@@ -46,19 +96,18 @@ const Map = (): React.ReactElement => {
       <Typography>location: {location.latitude}, {location.longitude}</Typography>
       {recommendation.map((attraction, i) => <AttractionPin attraction={attraction} index={i} key={i} />)}
       <Box>
-        <div>
+
           <Autocomplete>
             <Input type='text' placeholder='Origin' ref={originRef} />
           </Autocomplete>
-        </div>
-        <div>
+
           <Autocomplete>
             <Input
               type='text'
               placeholder='Destination'
-              ref={destiantionRef} />
+              ref={destinantionRef} />
           </Autocomplete>
-        </div>
+
         <Button
           onClick={() => {
             if (map != null) {
@@ -68,6 +117,13 @@ const Map = (): React.ReactElement => {
           }}
         >
           center back
+        </Button>
+        <Button
+          onClick={() => {
+            void (calculateRoute())
+          }}
+        >
+          route
         </Button>
       </Box>
       <GoogleMap
@@ -82,7 +138,9 @@ const Map = (): React.ReactElement => {
         }}
         onLoad={map => setMap(map)}
       >
+
         <Marker position={center} />
+        {directionsResponse !== undefined && <DirectionsRenderer directions={directionsResponse} />}
 
       </GoogleMap>
     </Box>
