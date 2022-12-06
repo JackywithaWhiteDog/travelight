f4f447324e262f88fc5a6af51fb27495bc2a851a
coffree0123 <02320013apis@gmail.com>
2022-12-06 15:42:10 +0800

    fix(backend): fix CORS problem

--- backend/src/main/java/com/travelight/backend/BackendApplication.java
+++ backend/src/main/java/com/travelight/backend/BackendApplication.java
@@ -1,13 +1,13 @@
 package com.travelight.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -23,8 +23,8 @@ public class BackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/optimize").allowedOrigins();
-				registry.addMapping("/nearbyAttractions").allowedOrigins();
+				registry.addMapping("/optimize");
+				registry.addMapping("/nearbyAttractions");
 			}
 		};
 	}
