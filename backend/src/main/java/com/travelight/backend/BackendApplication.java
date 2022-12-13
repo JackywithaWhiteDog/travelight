bdd5e703c8fc773bd7652744fdeac31cd3c3f8b9
JackywithaWhiteDog <jackyliu0129@gmail.com>
2022-12-13 10:57:51 +0800

    test(backend): or-tools dependency

--- backend/src/main/java/com/travelight/backend/BackendApplication.java
+++ backend/src/main/java/com/travelight/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.travelight.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,44 @@ public class BackendApplication {
         registry.addMapping("/nearbyAttractions");
       }
     };
+  }
+
+  @GetMapping("/test")
+  public String test() {
+    System.out.println("test:\n");
+
+    long[][] timeMatrixData = {
+        // {0, 0, 0, 0},
+        // {0, 0, 3, 40},
+        // {0, 5000, 0, 1},
+        // {0, 4, 1, 0},
+        { 0, 0, 0, 0 },
+        { 0, 0, 19, 24 },
+        { 0, 19, 0, 23 },
+        { 0, 24, 23, 0 }
+    };
+    long[][] timeWindowsData = {
+        // {0, 1440},
+        // {0, 10},
+        // {4, 8},
+        // {0, 12},
+        { 0, 1440 },
+        { 900, 1440 },
+        { 480, 1140 },
+        { 480, 1140 }
+    };
+    long[] stayTimes = { 60, 90, 60 };
+    TimeMatrix timeMatrix = new TimeMatrix(false, timeMatrixData);
+    TimeWindows timeWindows = new TimeWindows(timeWindowsData);
+
+    VrptwDataModel vrptwDataModel = new VrptwDataModel(timeMatrix, timeWindows, stayTimes);
+    vrptwDataModel.transformDataByStayTimes();
+    vrptwDataModel.timeMatrix.printMatrix();
+    vrptwDataModel.timeWindows.printWindows();
+
+    VrptwSolver.solve(vrptwDataModel);
+
+    return "completed";
   }
 
   @PostMapping("/optimize")
--- backend/src/main/java/com/travelight/backend/VrptwSolver.java
+++ backend/src/main/java/com/travelight/backend/VrptwSolver.java
@@ -1,6 +1,5 @@
 package com.travelight.backend;
 
-import com.google.ortools.Loader;
 import com.google.ortools.constraintsolver.Assignment;
 import com.google.ortools.constraintsolver.FirstSolutionStrategy;
 import com.google.ortools.constraintsolver.IntVar;
@@ -14,26 +13,26 @@ public class VrptwSolver {
 
   public static void main(String[] args) {
     long[][] timeMatrixData = {
-      // {0, 0, 0, 0},
-      // {0, 0, 3, 40},
-      // {0, 5000, 0, 1},
-      // {0, 4, 1, 0},
-      {0, 0, 0, 0},
-      {0, 0, 19, 24},
-      {0, 19, 0, 23},
-      {0, 24, 23, 0}
+        // {0, 0, 0, 0},
+        // {0, 0, 3, 40},
+        // {0, 5000, 0, 1},
+        // {0, 4, 1, 0},
+        { 0, 0, 0, 0 },
+        { 0, 0, 19, 24 },
+        { 0, 19, 0, 23 },
+        { 0, 24, 23, 0 }
     };
     long[][] timeWindowsData = {
-      // {0, 1440},
-      // {0, 10},
-      // {4, 8},
-      // {0, 12},
-      {0, 1440},
-      {900, 1440},
-      {480, 1140},
-      {480, 1140}
+        // {0, 1440},
+        // {0, 10},
+        // {4, 8},
+        // {0, 12},
+        { 0, 1440 },
+        { 900, 1440 },
+        { 480, 1140 },
+        { 480, 1140 }
     };
-    long[] stayTimes = {60, 90, 60};
+    long[] stayTimes = { 60, 90, 60 };
     TimeMatrix timeMatrix = new TimeMatrix(false, timeMatrixData);
     TimeWindows timeWindows = new TimeWindows(timeWindowsData);
 
@@ -46,27 +45,25 @@ public class VrptwSolver {
   }
 
   public static void solve(VrptwDataModel vrptwDataModel) {
-    Loader.loadNativeLibraries();
+    System.load("/usr/share/or-tools/libjniortools.so");
 
     // Create Routing Index Manager
-    RoutingIndexManager manager =
-        new RoutingIndexManager(
-            vrptwDataModel.timeMatrix.data.length,
-            vrptwDataModel.vehicleNumber,
-            vrptwDataModel.depot);
+    RoutingIndexManager manager = new RoutingIndexManager(
+        vrptwDataModel.timeMatrix.data.length,
+        vrptwDataModel.vehicleNumber,
+        vrptwDataModel.depot);
 
     // Create Routing Model.
     RoutingModel routing = new RoutingModel(manager);
 
     // Create and register a transit callback.
-    final int transitCallbackIndex =
-        routing.registerTransitCallback(
-            (long fromIndex, long toIndex) -> {
-              // Convert from routing variable Index to user NodeIndex.
-              int fromNode = manager.indexToNode(fromIndex);
-              int toNode = manager.indexToNode(toIndex);
-              return vrptwDataModel.timeMatrix.data[fromNode][toNode];
-            });
+    final int transitCallbackIndex = routing.registerTransitCallback(
+        (long fromIndex, long toIndex) -> {
+          // Convert from routing variable Index to user NodeIndex.
+          int fromNode = manager.indexToNode(fromIndex);
+          int toNode = manager.indexToNode(toIndex);
+          return vrptwDataModel.timeMatrix.data[fromNode][toNode];
+        });
 
     // Define cost of each arc.
     routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
@@ -98,10 +95,9 @@ public class VrptwSolver {
     routing.addVariableMinimizedByFinalizer(timeDimension.cumulVar(routing.end(vehicle)));
 
     // Setting first solution heuristic.
-    RoutingSearchParameters searchParameters =
-        main.defaultRoutingSearchParameters().toBuilder()
-            .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
-            .build();
+    RoutingSearchParameters searchParameters = main.defaultRoutingSearchParameters().toBuilder()
+        .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+        .build();
 
     // Solve the problem.
     Assignment solution = routing.solveWithParameters(searchParameters);
@@ -121,23 +117,21 @@ public class VrptwSolver {
     String route = "";
     while (!routing.isEnd(index)) {
       IntVar timeVar = timeDimension.cumulVar(index);
-      route +=
-          manager.indexToNode(index)
-              + " Time("
-              + solution.min(timeVar)
-              + ","
-              + solution.max(timeVar)
-              + ") -> ";
+      route += manager.indexToNode(index)
+          + " Time("
+          + solution.min(timeVar)
+          + ","
+          + solution.max(timeVar)
+          + ") -> ";
       index = solution.value(routing.nextVar(index));
     }
     IntVar timeVar = timeDimension.cumulVar(index);
-    route +=
-        manager.indexToNode(index)
-            + " Time("
-            + solution.min(timeVar)
-            + ","
-            + solution.max(timeVar)
-            + ")";
+    route += manager.indexToNode(index)
+        + " Time("
+        + solution.min(timeVar)
+        + ","
+        + solution.max(timeVar)
+        + ")";
 
     // Display the route
     System.out.println(route);
